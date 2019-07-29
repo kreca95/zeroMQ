2e728a2f9503c56bfbcdb685cc8c8d406899972d
Kresimir Sutalo <kresimir095@gmail.com>
2019-07-29 09:41:02 +0200

    first commit2

---
+++ SubConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace Examples
+{
+    static partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> poruke = new List<string>();
+            using (var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5555");
+                while (true)
+                {
+                    var message = server.ReceiveFrameString();
+                    poruke.Add(message);
+                    //Console.WriteLine("Received {0}", message);
+                    Thread.Sleep(100);
+
+                    foreach (var item in poruke)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    server.SendFrame(message);
+                }
+            }
+
+        }
+    }
+}
---
+++ consoleChat/Program.cs
@@ -0,0 +1,36 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using ZeroMQ;
+
+namespace Examples
+{
+    static partial class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite nick");
+            string nick = Console.ReadLine();
+            using (var client = new RequestSocket())
+            {
+                client.Connect("tcp://localhost:5555");
+                for (int i = 0; i < 10; i++)
+                {
+
+                    Console.WriteLine("-----");
+                    var mess = Console.ReadLine();
+                    client.SendFrame(nick + " "+mess);
+                    var message = client.ReceiveFrameString();
+                    Console.WriteLine("Received {0}", message);
+
+                }
+            }
+        }
+    }
+}
